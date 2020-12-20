8c46de9c51ee6a439571526da27b4f1b24381e90
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2020-12-20 22:39:11 +0300

    getting ready to code 🐱‍🐉

---
+++ components/AppMenu.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <ul>
+      <li><nuxt-link to="/" exact>Home</nuxt-link></li>
+      <li><nuxt-link to="/about">About</nuxt-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
